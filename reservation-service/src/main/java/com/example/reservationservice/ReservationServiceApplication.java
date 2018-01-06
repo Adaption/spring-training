f038651d864026d8e7f97dd0dd25a688fc80a995
nhat7490 <nhatnqse62245@fpt.edu.vn>
2018-01-06 23:15:05 +0700

    Config_training

--- reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -3,10 +3,12 @@ package com.example.reservationservice;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,16 +29,32 @@ public class ReservationServiceApplication {
 }
 
 @RestController
-class ReservationRestApiController{
+@RefreshScope
+class MessageRestController {
+
+    private final String value;
+
+    MessageRestController(@Value("${message}") String value) {
+        this.value = value;
+    }
+
+    @GetMapping("/message")
+    String read() {
+        return this.value;
+    }
+}
+
+@RestController
+class ReservationRestApiController {
 
     private final ReservationRepository reservationRepository;
 
-    ReservationRestApiController(ReservationRepository reservationRepository){
+    ReservationRestApiController(ReservationRepository reservationRepository) {
         this.reservationRepository = reservationRepository;
     }
 
     @GetMapping("/reservation")
-    Collection<Reservation> reservations(){
+    Collection<Reservation> reservations() {
         return this.reservationRepository.findAll();
     }
 }
