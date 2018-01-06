317fe1c99f688b78fcbe2bae9f28d328b8960afe
TuanDSE62171 <heryhudson@gmail.com>
2018-01-06 12:21:35 +0700

    added eureka  service and reservation client

--- reservation-service/src/main/java/reservationservice/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/reservationservice/reservationservice/ReservationServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -17,6 +18,7 @@ import java.util.stream.Stream;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.ServerResponse.*;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
 
