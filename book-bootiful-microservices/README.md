857bc2a3c5f65fb699a694bb2351c2e7f5a196e7
swomfire <vucuongJim@gmail.com>
2018-01-10 13:32:53 +0700

    Add Config Server, Update README

--- book-bootiful-microservices/README.md
+++ book-bootiful-microservices/README.md
@@ -1,4 +1,6 @@
 # spring-training
 Training for Building Microservices using Spring
 # Services - Dependencies
-- book-service - Web, H2, Config Client, Eureka Discovery, Zipkin Client, Stream Rabbit, Rest Repositories, JPA, Actuator
+- book-service - Web, H2, Config Client, Eureka Discovery, Zipkin Client, Stream Rabbit, Rest Repositories, JPA, Actuator, Lombok
+- config-server - Config Server
+	-> the config is at https://github.com/swomfire/book-bootiful-microservices-config
--- book-bootiful-microservices/book-service/src/main/java/com/example/bookservice/BookServiceApplication.java
+++ book-bootiful-microservices/book-service/src/main/java/com/example/bookservice/BookServiceApplication.java
@@ -3,12 +3,18 @@ package com.example.bookservice;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -22,6 +28,22 @@ public class BookServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookServiceApplication.class, args);
+    }
+}
+
+@RestController
+@RefreshScope
+class MessageRestController{
+    private final String value;
+
+    @Autowired
+    public MessageRestController(@Value("${message}") String value) {
+        this.value = value;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/message")
+    String read(){
+        return this.value;
     }
 }
 
